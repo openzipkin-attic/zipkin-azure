f8321f9f9864bfea48aeb90a2ad474ea3a91eb96
Adrian Cole <acole@pivotal.io>
2017-02-03 14:34:19 +0800

    Refactors directory structure, notably eventhubs, not eventhub
    The name of the Azure service is "Event Hubs", not Eventhub or similar.
    I've renamed package and config names to match Microsoft's convention,
    which is interestingly inconsistent between services. For example,
    "IoT Hub" and "Event Hubs"
    
    I also removed the "module" package in favor of collector. "module" is
    a side-effect of the autoconfiguration build.
    
    Finally, I cleaned up some sentences.

--- README.md
+++ README.md
@@ -14,29 +14,29 @@ the [zipkin-reporters-java](https://github.com/openzipkin/zipkin-reporter-java) 
 
 ## Collectors
 
-### EventHub Collector
-EventHub is a Zipkin collecor that can collect spans from Azure EventHub, instead of Kafka. [Azure EventHub](https://azure.microsoft.com/en-us/services/event-hubs/) is an Azure PaaS Service has a functionality (and design) very similar to Apache Kafka where on one hand, the data is be pushed to a sink. On the other hand, the data is read by the consumers from partitioned storage where only a single consumer reads from a partition guaranteeing ordering of the messages. On regular intervals, partition gets checkpointed.
+### Azure Event Hubs Collector
+The Zipkin Azure Event Hubs Collector is an alternative to Kafka. The [Azure Event Hubs](https://azure.microsoft.com/en-us/services/event-hubs/) service is similar to Apache Kafka where data is be pushed to a sink. On the other hand, single consumer reads from a partition have ordering guarantees. These partitions are check pointed on regular intervals.
 
 See **Server Integration** for more info how to run it.
 
 ## Server integration
 In order to integrate with zipkin-server, you need to use properties launcher to load your collector (or sender) alongside the zipkin-server process.
 
-This stepwise guide, documents how to do this for EventHub Collector.
+Here's an example of integrating the Azure Event Hubs Collector:
 
 ### 1- Clone the source and build
+Until the first version is published, you need to build the collector locally.
 ``` bash
-mkdir zipkin-collector-eventhub
-cd zipkin-collector-eventhub
-git clone git@github.com:openzipkin/zipkin-azure.git
-mvn package
+mkdir zipkin-collector-eventhubs
+cd zipkin-collector-eventhubs
+git clone https://github.com/openzipkin/zipkin-azure.git
+./mvnw package
 ```
-If you do not have maven, get maven [here](http://maven.apache.org/install.html).
 
 ### 2- Unpackage MODULE jar into an empty folder
-copy zipkin-collector-eventhub-autoconfig-x.x.x-SNAPSHOT-module.jar (that has been package in the `target` folder) into an empty folder and unpackage
+copy zipkin-collector-eventhubs-autoconfig-x.x.x-SNAPSHOT-module.jar (that has been package in the `target` folder) into an empty folder and unpackage
 ``` bash
-jar xf zipkin-collector-eventhub-autoconfig-0.1.0-SNAPSHOT-module.jar
+jar xf zipkin-collector-eventhubs-autoconfig-0.1.0-SNAPSHOT-module.jar
 ```
 You may then delete the jar itself.
 
@@ -44,18 +44,18 @@ You may then delete the jar itself.
 Download the latest zipkin-server jar (which is named zipkin.jar) from [here](https://search.maven.org/remote_content?g=io.zipkin.java&a=zipkin-server&v=LATEST&c=exec). For more information visit [zipkin-server homepage](https://github.com/openzipkin/zipkin/tree/master/zipkin-server).  
 
 ### 4- create an `application.properties` file for configuration next to the zipkin.jar file
-Populate the configuration - make sure the resources (Azure Storage, EventHub, etc) exist. **Only storageConnectionString is mandatory** the rest are optional and must be used only to override the defaults:
+Populate the configuration - make sure the resources (Azure Storage, Event Hubs, etc) exist. **Only storageConnectionString is mandatory** the rest are optional and must be used only to override the defaults:
 ```
-zipkin.collector.eventhub.storageConnectionString=<azure storage connection string>
-zipkin.collector.eventhub.eventHubName=<name of the eventhub, default is zipkin>
-zipkin.collector.eventhub.consumerGroupName=<name of the consumer group, default is $Default>
-zipkin.collector.eventhub.storageContainerName=<name of the storage container, default is zipkin>
-zipkin.collector.eventhub.processorHostName=<name of the processor host, default is a randomly generated GUID>
-zipkin.collector.eventhub.storageBlobPrefix=<the path within container where blobs are created for partition lease, processorHostName>
+zipkin.collector.eventhubs.storageConnectionString=<azure storage connection string>
+zipkin.collector.eventhubs.eventHubName=<name of the eventhubs, default is zipkin>
+zipkin.collector.eventhubs.consumerGroupName=<name of the consumer group, default is $Default>
+zipkin.collector.eventhubs.storageContainerName=<name of the storage container, default is zipkin>
+zipkin.collector.eventhubs.processorHostName=<name of the processor host, default is a randomly generated GUID>
+zipkin.collector.eventhubs.storageBlobPrefix=<the path within container where blobs are created for partition lease, processorHostName>
 ```
 
 ### 5- Run the server along with the collector
 Assuming `zipkin.jar` and `application.properties` are in the current working directory. Note that the EventHub connection string gets passed as a command-line parameter, not from the `application.properties` file:
 ```
-java -Dloader.path=/where/jar/was/unpackaged -cp zipkin.jar org.springframework.boot.loader.PropertiesLauncher --spring.config.location=application.properties --zipkin.collector.eventhub.eventHubConnectionString="<eventhub connection string, make sure quoted otherwise won't work>"
+java -Dloader.path=/where/jar/was/unpackaged -cp zipkin.jar org.springframework.boot.loader.PropertiesLauncher --spring.config.location=application.properties --zipkin.collector.eventhubs.eventHubConnectionString="<eventhubs connection string, make sure quoted otherwise won't work>"
 ```
---
+++ collector/eventhubs/src/main/java/zipkin/collector/eventhubs/EventHubCollector.java
@@ -0,0 +1,212 @@
+/**
+ * Copyright 2017 The OpenZipkin Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+/**
+ * Created by aliostad on 14/01/2017.
+ */
+package zipkin.collector.eventhubs;
+
+
+import com.microsoft.azure.eventprocessorhost.EventProcessorHost;
+import zipkin.collector.Collector;
+import zipkin.collector.CollectorComponent;
+import zipkin.collector.CollectorMetrics;
+import zipkin.collector.CollectorSampler;
+import zipkin.storage.StorageComponent;
+
+import java.io.IOException;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class EventHubCollector implements CollectorComponent {
+
+  private Builder builder;
+  private EventProcessorHost host;
+  private Boolean started = false;
+  private Exception lastException;
+
+  private static final Logger logger = Logger.getLogger(EventHubCollector.class.getName());
+
+  public EventHubCollector(Builder builder) {
+    this.builder = builder;
+    logger.log(Level.INFO,"_____EventHubCollector_ctor_____");
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  public static final class Builder implements CollectorComponent.Builder {
+    Collector.Builder delegate = Collector.builder(EventHubCollector.class);
+    CollectorMetrics metrics = CollectorMetrics.NOOP_METRICS;
+    String eventHubName = "zipkin";
+    String consumerGroupName = "$Default";
+    String eventHubConnectionString;
+    String storageConnectionString;
+    String storageContainerName = "zipkin";
+    int checkpointBatchSize = 10;
+
+    String processorHostName = UUID.randomUUID().toString();
+    String storageBlobPrefix = processorHostName;
+
+
+    public Builder() {
+      System.out.println("_____builder______");
+    }
+
+    public EventHubCollector.Builder eventHubName(String name) {
+      eventHubName = name;
+      return this;
+    }
+
+    public EventHubCollector.Builder consumerGroupName(String name) {
+      consumerGroupName = name;
+      return this;
+    }
+
+    public EventHubCollector.Builder checkpointBatchSize(int size) {
+      checkpointBatchSize = size;
+      return this;
+    }
+
+    public EventHubCollector.Builder eventHubConnectionString(String connectionString) {
+      eventHubConnectionString = connectionString;
+      return this;
+    }
+
+    public EventHubCollector.Builder storageConnectionString(String connectionString) {
+      storageConnectionString = connectionString;
+      return this;
+    }
+
+    public EventHubCollector.Builder storageContainerName(String containerName) {
+      storageContainerName = containerName;
+      return this;
+    }
+
+    public EventHubCollector.Builder storageBlobPrefix(String blobPrefix) {
+      storageBlobPrefix = blobPrefix;
+      return this;
+    }
+
+    public EventHubCollector.Builder processorHostName(String nameForThisProcessorHost) {
+      processorHostName = nameForThisProcessorHost;
+      return this;
+    }
+
+    public EventHubCollector.Builder storage(StorageComponent storage) {
+      delegate.storage(storage);
+      return this;
+    }
+
+    public EventHubCollector.Builder metrics(CollectorMetrics metrics) {
+      delegate.metrics(metrics);
+      return this;
+    }
+
+    public EventHubCollector.Builder sampler(CollectorSampler sampler) {
+      delegate.sampler(sampler);
+      return this;
+    }
+
+    public EventHubCollector build() {
+      return new EventHubCollector(this);
+    }
+  }
+
+  public String getEventHubName() {
+    return builder.eventHubName;
+  }
+
+  public String getConsumerGroupName() {
+    return builder.consumerGroupName;
+  }
+
+  public String getEventHubConnectionString() {
+    return builder.eventHubConnectionString;
+  }
+
+  public String getStorageConnectionString() {
+    return builder.storageConnectionString;
+  }
+
+  public String getStorageContainerName() {
+    return builder.storageContainerName;
+  }
+
+  public int getCheckpointBatchSize() {
+    return builder.checkpointBatchSize;
+  }
+
+  public String getProcessorHostName() {
+    return builder.processorHostName;
+  }
+
+  public String getStorageBlobPrefix() {
+    return builder.storageBlobPrefix;
+  }
+
+  public EventHubCollector start() {
+
+    logger.log(Level.INFO,"_____EventHubCollector_start______");
+    try {
+
+      logger.log(Level.INFO, "processorHostName: " + builder.processorHostName);
+      logger.log(Level.INFO, "consumerGroupName: " + builder.consumerGroupName);
+      logger.log(Level.INFO, "eventHubName: " + builder.eventHubName);
+      logger.log(Level.INFO, "eventHubConnectionString: " + builder.eventHubConnectionString);
+      logger.log(Level.INFO, "storageConnectionString: " + builder.storageConnectionString);
+      logger.log(Level.INFO, "storageContainerName: " + builder.storageContainerName);
+      logger.log(Level.INFO, "storageBlobPrefix: " + builder.storageBlobPrefix);
+
+      host = new EventProcessorHost(builder.processorHostName,
+          builder.eventHubName, builder.consumerGroupName,
+          builder.eventHubConnectionString, builder.storageConnectionString,
+          builder.storageContainerName, builder.storageBlobPrefix);
+
+      host.registerEventProcessorFactory(
+          context -> new ZipkinEventProcessor(builder)
+      );
+      started = true;
+    } catch (Exception e) {
+      e.printStackTrace();
+      lastException = e;
+
+      logger.log(Level.WARNING,"_____EventHubCollector_exception____");
+      System.out.println(e);
+
+    }
+    return this;
+  }
+
+  public CheckResult check() {
+    logger.log(Level.INFO,"_____EventHubCollector_check____");
+    return started || lastException == null ? CheckResult.OK : CheckResult.failed(lastException);
+  }
+
+  public void close() throws IOException {
+    try {
+      logger.log(Level.INFO,"_____EventHubCollector_close_____");
+      if (host != null) {
+        host.unregisterEventProcessor();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      lastException = e;
+    }
+
+    started = false;
+  }
+}
