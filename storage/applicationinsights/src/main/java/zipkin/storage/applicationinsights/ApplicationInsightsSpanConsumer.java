78a8ebd7abe80096951bdf798ed9965d52b42afa
Praveen Barli <prbarl@microsoft.com>
2017-06-26 16:25:31 -0700

    data model changes to add name, id and dependency for sr annotaiton

--- storage/applicationinsights/src/main/java/zipkin/storage/applicationinsights/ApplicationInsightsSpanConsumer.java
+++ storage/applicationinsights/src/main/java/zipkin/storage/applicationinsights/ApplicationInsightsSpanConsumer.java
@@ -16,13 +16,16 @@ package zipkin.storage.applicationinsights;
 import com.microsoft.applicationinsights.TelemetryClient;
 import com.microsoft.applicationinsights.telemetry.SeverityLevel;
 import com.microsoft.applicationinsights.TelemetryConfiguration;
+import com.microsoft.applicationinsights.telemetry.Duration;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
+import zipkin.Annotation;
 import zipkin.Span;
 import zipkin.storage.StorageAdapters;
+import zipkin.Constants;
 
 import static zipkin.internal.ApplyTimestampAndDuration.guessTimestamp;
 
@@ -48,11 +51,17 @@ final class ApplicationInsightsSpanConsumer implements StorageAdapters.SpanConsu
         Span span = spans.get(i);
         Map<String, String> spanProps = new HashMap<String, String>();
         //set indexing properties, avoid null values for props
-        spanProps.put("spanid", Long.toString(span.id));
-        spanProps.put("traceId", Long.toString(span.traceId));
-        spanProps.put("traceIdHigh", Long.toString(span.traceIdHigh));
+
+        String spanId = Long.toString(span.id);
+        String traceId = Long.toString(span.traceId);
+        String traceIdHigh = Long.toString(span.traceIdHigh);
+        String namespace = (this.namespace == null) ? "" : this.namespace;
+
+        spanProps.put("spanid", spanId);
+        spanProps.put("traceId", traceId);
+        spanProps.put("traceIdHigh", traceIdHigh);
         //namespace to support duplicate data
-        spanProps.put("namespace", (this.namespace == null)?"":this.namespace);
+        spanProps.put("namespace", namespace);
         if (span.annotations != null
             && span.annotations.size() > 0
             && span.annotations.get(0).endpoint != null
@@ -68,6 +77,16 @@ final class ApplicationInsightsSpanConsumer implements StorageAdapters.SpanConsu
         }
         String res = gson.toJson(jsonElement);
         String msg = "{ \"Span\":" + res + "}";
+        //data model changes
+        telemetry.getContext().getOperation().setId(traceId);
+        telemetry.getContext().getOperation().setName(span.name);
+
+        for (Annotation annotation : span.annotations) {
+          if (annotation.value.equalsIgnoreCase(Constants.SERVER_RECV)) {
+            telemetry.trackDependency(Constants.SERVER_RECV, "request", new Duration(span.duration==null?0L:span.duration),
+                true);
+          }
+        }
         telemetry.trackTrace(msg, SeverityLevel.Critical, spanProps);
       }
 
