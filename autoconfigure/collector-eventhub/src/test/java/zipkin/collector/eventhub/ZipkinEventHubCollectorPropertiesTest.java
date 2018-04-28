6f8e8d3f6222697eb9f849b9aa2fa06919e5c397
Adrian Cole <acole@pivotal.io>
2018-04-28 20:26:11 +0800

    Updates to spring boot 2

--- autoconfigure/collector-eventhub/src/test/java/zipkin/collector/eventhub/ZipkinEventHubCollectorPropertiesTest.java
+++ autoconfigure/collector-eventhub/src/test/java/zipkin/collector/eventhub/ZipkinEventHubCollectorPropertiesTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017 The OpenZipkin Authors
+ * Copyright 2017-2018 The OpenZipkin Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -19,7 +19,7 @@ import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.springframework.boot.autoconfigure.PropertyPlaceholderAutoConfiguration;
+import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Configuration;
